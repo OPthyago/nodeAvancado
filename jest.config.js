53507216493cee597dc9a87a4ddc014f32750979
Thyago de Oliveira Pereira <thyagoop@gmail.com>
2022-04-03 20:52:01 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: '<rootDir>/src/**/*.ts',
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+  moduleNameMapper: {
+    '@/tests(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
