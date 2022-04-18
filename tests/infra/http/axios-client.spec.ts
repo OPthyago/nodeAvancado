e554962e6298fea656ce427401ba2b1da21870c9
Thyago de Oliveira Pereira <thyagoop@gmail.com>
2022-04-17 21:13:51 -0300

    isolate sut creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -11,7 +11,17 @@ class AxiosHttpClient {
 
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
-  const fakeAxios = axios as jest.Mocked<typeof axios>
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: Object
+
+  beforeAll(() => {
+    fakeAxios = axios as jest.Mocked<typeof axios>
+    url = 'any_url'
+    params = {
+      any: 'any'
+    }
+  })
 
   beforeEach(() => {
     sut = new AxiosHttpClient()
@@ -19,18 +29,9 @@ describe('AxiosHttpClient', () => {
 
   describe('GET', () => {
     it('should call get with correct params', async () => {
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
-      })
+      await sut.get({ url, params })
 
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any'
-        }
-      })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
   })
